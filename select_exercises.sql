b5fd37a0f1dea87ee8d4ffc8708eaac2e9bea7e0
Abraham Garcia <aberaham.garcia55@gmail.com>
2023-11-27 16:21:01 -0600

    albums_seeder.sql & select_exercises.sql: completed exercises

---
+++ select_exercises.sql
@@ -0,0 +1,13 @@
+USE hippo_abe;
+    SELECT name FROM Albums WHERE artist = 'Pink Floyd';
+
+    SELECT release_date FROM Albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+
+    SELECT genre FROM Albums WHERE name = 'The Dark Side of the Moon';
+
+    SELECT name FROM Albums WHERE  release_date = 1990;
+
+    SELECT name FROM Albums WHERE sales < 40;
+
+    SELECT name FROM Albums WHERE genre = 'Hard Rock';
+
