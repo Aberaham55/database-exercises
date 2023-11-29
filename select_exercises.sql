b33c35465f2ef1e4a72a4e025be8f4b3a6ea57af
Abraham Garcia <aberaham.garcia55@gmail.com>
2023-11-28 20:34:38 -0600

    where_exercises.sql: completed exercise

--- select_exercises.sql
+++ select_exercises.sql
@@ -5,9 +5,9 @@ USE hippo_abe;
 
     SELECT genre FROM Albums WHERE name = 'The Dark Side of the Moon';
 
-    SELECT name FROM Albums WHERE  release_date = 1990;
+    SELECT name FROM Albums WHERE  release_date BETWEEN 1990 and 1999
 
     SELECT name FROM Albums WHERE sales < 40;
 
-    SELECT name FROM Albums WHERE genre = 'Hard Rock';
+    SELECT name FROM Albums WHERE genre = 'Rock';
 
---
+++ where_exercises.sql
@@ -0,0 +1,21 @@
+use employees
+
+select * from employees
+where first_name in ('Irena', 'Vidya', 'Maya')
+
+select * from employees
+where first_name in ('Irena', 'Vidya', 'Maya') and gender = 'M'
+
+select * from employees
+where last_name like ('e%') or
+        last_name like ('%e');
+
+select * from employees
+where last_name like ('e%') and
+        last_name like ('%e');
+
+select * from employees
+where last_name like ('%q%')
+  and last_name NOT LIKE '%qu%';
+
+
